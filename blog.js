ed52a5d7521f394429f659ba416832d079aa20e3
NathanPro1 <149512774+NathanPro1@users.noreply.github.com>
2023-12-26 19:57:14 +0300

    Add files via upload

---
+++ blog.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('active');
+    this.classList.toggle('active');
+  });
+});
